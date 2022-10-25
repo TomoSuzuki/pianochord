6fd5d3eed2f8595218297c61e3d06d3fb0f90b0f
mbp2021early <provisionale2@mbp2021early.local>
2022-10-25 17:32:28 +0900

    midi test

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_midi/flutter_midi.dart';
+import 'package:flutter/foundation.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  final _flutterMidi = FlutterMidi();
+  @override
+  void initState() {
+    if (!kIsWeb) {
+      load(_value);
+    } else {
+      _flutterMidi.prepare(sf2: null);
+    }
+    super.initState();
+  }
+
+  void load(String asset) async {
+    print('Loading File...');
+    _flutterMidi.unmute();
+    ByteData _byte = await rootBundle.load(asset);
+    //assets/sf2/SmallTimGM6mb.sf2
+    //assets/sf2/Piano.SF2
+    _flutterMidi.prepare(sf2: _byte, name: _value.replaceAll('assets/', ''));
+  }
+
+  String _value = 'assets/Piano.sf2';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            // DropdownButton<String>(
+            //   value: _value,
+            //   items: [
+            //     DropdownMenuItem(
+            //       child: Text("Soft Piano"),
+            //       value: "assets/sf2/SmallTimGM6mb.sf2",
+            //     ),
+            //     DropdownMenuItem(
+            //       child: Text("Loud Piano"),
+            //       value: "assets/sf2/Piano.SF2",
+            //     ),
+            //   ],
+            //   onChanged: (String value) {
+            //     setState(() {
+            //       _value = value;
+            //     });
+            //     load(_value);
+            //   },
+            // ),
+            ElevatedButton(
+              child: Text('Play C'),
+              onPressed: () {
+                _play(60);
+              },
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        )),
       ),
     );
+  }
+
+  void _play(int midi) {
+    if (kIsWeb) {
+      // WebMidi.play(midi);
+    } else {
+      _flutterMidi.playMidiNote(midi: midi);
+    }
   }
 }
