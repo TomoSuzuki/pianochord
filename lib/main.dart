16b523d7876459a4d0fbea2e4df09e6c04a52bc9
mbp2021early <provisionale2@mbp2021early.local>
2022-10-31 17:03:08 +0900

    packagePiano

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,57 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_midi/flutter_midi.dart';
+import 'package:piano/piano.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _flutterMidi = FlutterMidi();
+  @override
+  void initState() {
+    load(_value);
+    super.initState();
+  }
+
+  void load(String asset) async {
+    _flutterMidi.unmute(); // Optionally Unmute
+    ByteData _byte = await rootBundle.load(asset);
+    _flutterMidi.prepare(sf2: _byte, name: _value.replaceAll('assets/', ''));
+  }
+
+  String _value = 'assets/Yamaha-Grand-Lite-v2.0.sf2';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    return CupertinoApp(
+        title: 'Piano Demo',
+        home: Center(
+          child: InteractivePiano(
+            highlightedNotes: [NotePosition(note: Note.C, octave: 3)],
+            naturalColor: Colors.white,
+            accidentalColor: Colors.black,
+            keyWidth: 60,
+            noteRange: NoteRange.forClefs([
+              Clef.Treble,
+            ]),
+            onNotePositionTapped: (position) {
+              //print(position);
+              _play(60);
+              // Use an audio library like flutter_midi to play the sound
+            },
+          ),
+        ));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+  void _play(int midi) {
+    _flutterMidi.playMidiNote(midi: midi);
   }
 }
